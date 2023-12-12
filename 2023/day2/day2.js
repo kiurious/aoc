424e8ec2f9df59ba98da30c1e2078532949a6aee
kiurious <kiurinegrao@gmail.com>
2023-12-12 18:25:51 +0200

    added needed vars and string manipulation. to continue tomorrow.

--- 2023/day2/day2.js
+++ 2023/day2/day2.js
@@ -1,8 +1,25 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("./input.txt", "utf8");
+const input = fs.readFileSync("./example1.txt", "utf8");
 const games = input.trim().split("\n");
 
 const maxRedCubes = 12;
 const maxGreenCubes = 13;
 const maxBlueCubes = 14;
+
+function partOne() {
+  const values = games.map((game) => {
+    let redCubes = 0;
+    let greenCubes = 0;
+    let blueCubes = 0;
+    let gameID = game.split(" ")[1].split(":")[0];
+    let setGroup = game.split(":")[1].split(";");
+
+    console.log(setGroup);
+    setGroup.forEach((set) => {
+      // add something here tomorrow
+    });
+  });
+}
+
+console.log(partOne());
