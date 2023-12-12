4949a852e7ef9b31f98f575d16583835f9d2ac05
kiurious <kiurinegrao@gmail.com>
2023-12-12 16:41:19 +0200

    finished day 1

--- day1.js
+++ day1.js
@@ -8,9 +8,10 @@
 
 const fs = require("fs");
 
-function partOne(file) {
-  const input = fs.readFileSync(file, "utf8");
-  const lines = input.trim().split("\n");
+const input = fs.readFileSync("./input.txt", "utf8");
+const lines = input.trim().split("\n");
+
+function partOne() {
   const values = lines.map((line) => {
     let firstDigit = null;
     let lastDigit = null;
@@ -35,4 +36,98 @@ function partOne(file) {
   return values.reduce((sum, value) => sum + value);
 }
 
-console.log(partOne("./example.txt"));
+// console.log(partOne("./example.txt"));
+
+// Part Two
+// Some of the digits are spelled out with letters; "one", "two", "three", etc.
+// Find the real first and last digits.
+
+const spelledOutNumbers = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+function partTwo() {
+  const values = lines.map((line) => {
+    let firstSpelledDigit = null;
+    let firstSpelledDigitIndex = null;
+    let firstDigit = null;
+    let firstDigitIndex = null;
+    let lastSpelledDigit = null;
+    let lastDigit = null;
+    let lastDigitIndex = -1;
+    let tempArray = [];
+
+    // Process spelled-out numbers by going though each line and number in the object and finding all instances, assigning the index and the value of it.
+    Object.keys(spelledOutNumbers).forEach((number) => {
+      let startIndex = 0;
+      while (startIndex < line.length) {
+        let foundIndex = line.indexOf(number, startIndex);
+        if (foundIndex !== -1) {
+          tempArray.push({ number: number, index: foundIndex });
+          startIndex = foundIndex + number.length;
+        } else {
+          break;
+        }
+      }
+    });
+
+    // Sort by index to correctly order the digits.
+    tempArray.sort((a, b) => a.index - b.index);
+
+    // Assign spelled-out numbers to its variables
+    if (tempArray.length > 0) {
+      firstSpelledDigitIndex = tempArray[0].index;
+      firstSpelledDigit = spelledOutNumbers[tempArray[0].number];
+      lastSpelledDigit =
+        spelledOutNumbers[tempArray[tempArray.length - 1].number];
+    }
+
+    // Process non spelled-out digits -- adapted from part one
+    for (let i = 0; i < line.length; i++) {
+      if (!isNaN(parseInt(line[i]))) {
+        if (firstDigit === null) {
+          firstDigit = line[i];
+          firstDigitIndex = i;
+        }
+        lastDigit = line[i];
+        lastDigitIndex = i;
+      }
+    }
+
+    // Determine the first and last digits based on which index is first.
+    let finalFirstDigit =
+      firstDigitIndex !== null &&
+      (firstSpelledDigitIndex === null ||
+        firstDigitIndex < firstSpelledDigitIndex)
+        ? firstDigit
+        : firstSpelledDigit;
+    let finalLastDigit =
+      lastDigitIndex > -1 &&
+      (lastSpelledDigit === null ||
+        lastDigitIndex > tempArray[tempArray.length - 1].index)
+        ? lastDigit
+        : lastSpelledDigit;
+
+    console.log(`First: ${finalFirstDigit}, Last: ${finalLastDigit}`);
+
+    // Return calculation or 0 if no valid digits found
+    return finalFirstDigit && finalLastDigit
+      ? Number(finalFirstDigit.toString() + finalLastDigit.toString())
+      : 0;
+  });
+
+  // Final Sum
+  let sum = values.reduce((sum, value) => sum + value, 0);
+  console.log(`Total Sum: ${sum}`);
+  return sum;
+}
+
+console.log(partTwo());
